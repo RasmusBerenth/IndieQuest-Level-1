ccebf8ce31a5b3b5919c1f13d72708509f20db45
RasmusBerenth <98969083+RasmusBerenth@users.noreply.github.com>
2022-02-09 17:51:27 +0100

    Finished mission for Enums

---
+++ Enums/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Enums
+{
+    enum Suits
+    {
+        Heart,
+        Spades,
+        Diamonds,
+        Clover,
+    }
+    internal class Program
+    {
+        static void DrawAce(Suits suit)
+        {
+            //Other alternativs
+            //var suitSymbols = new List<string> { "♥", "♠", "♦", "♣" };
+            //var suitSymbolsArray = new string[] { "♥", "♠", "♦", "♣" };
+            //var suitSymbolsArrayChar = new char[] { '♥', '♠', '♦', '♣' };
+            string suitSymbols = "♥♠♦♣";
+
+            char suitSymbol = suitSymbols[(int)suit];
+            string ace = @"
+╭───────╮
+│A      │
+│♠      │
+│   ♠   │
+│      ♠│
+│      A│
+╰───────╯
+";
+
+            Console.Write(ace.Replace('♠', suitSymbol));
+
+        }
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            for (int i = 0; i < 4; i++)
+            {
+                DrawAce((Suits)i);
+            }
+            
+            //Other alternativs
+            //for (Suits i = Suits.Heart; i <= Suits.Clover; i++)
+            //{
+            //    DrawAce(i);
+            //}
+
+        }
+    }
+}
