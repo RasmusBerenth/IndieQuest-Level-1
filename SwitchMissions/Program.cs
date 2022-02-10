369d4b8f1b544f2f4027b845a5bcb4af968754eb
RasmusBerenth <98969083+RasmusBerenth@users.noreply.github.com>
2022-02-10 16:32:03 +0100

    completed mission 2

--- SwitchMissions/Program.cs
+++ SwitchMissions/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace SwitchMissions
 {
@@ -9,7 +8,7 @@ namespace SwitchMissions
         {
             Console.WriteLine("Set price:");
             string equation = Console.ReadLine();
-            string[] equationParts = equation.Split(' '); //list first double[0] symbol[1] second double[2]
+            string[] equationParts = equation.Split(' '); //list first double[0] symbol[1] second double[2] --> first double[0] symbol[1] symbol [2] second double[3]
             double price;
             double firstNumber = Convert.ToDouble(equationParts[0]);
 
@@ -20,28 +19,42 @@ namespace SwitchMissions
             else
             {
                 string symbol = equationParts[1];
-                double secondNumber = Convert.ToDouble(equationParts[2]);
+
+                if (equationParts.Length == 4)
+                {
+                    symbol = $"{equationParts[1]} {equationParts[2]}";
+                }
+
+                double secondNumber = Convert.ToDouble(equationParts[equationParts.Length-1]);
+
 
 
                 switch (symbol)
                 {
-                    //addition if price contains +
+                    //addition if price contains +, plus or add
                     case "+":
+                    case "plus":
+                    case "add":
                         price = firstNumber + secondNumber;
                         break;
 
-                    //subtraction if price contains -
+                    //subtraction if price contains -, minus or subtract
                     case "-":
+                    case "subtract":
+                    case "minus":
                         price = firstNumber - secondNumber;
                         break;
-
-                    //multiplication if price contains *
+                    
+                    //multiplication if price contains *, times or multiply by
                     case "*":
+                    case "times":
+                    case "multiplied by":
                         price = firstNumber * secondNumber;
                         break;
-
-                    //division if price contains /
+                       
+                    //division if price contains / or divided by
                     case "/":
+                    case "divided by":
                         price = firstNumber / secondNumber;
                         break;
 
@@ -50,8 +63,8 @@ namespace SwitchMissions
                         return;
                 }
             }
-                Console.WriteLine($"The price was set to: {price}");
 
+            Console.WriteLine($"The price was set to: {price}");
 
 
         }
