95ac702868e32db4616e72060a3653f515d07a6f
RasmusBerenth <98969083+RasmusBerenth@users.noreply.github.com>
2022-02-10 18:02:05 +0100

    added an enum

---
+++ ArraysMission1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ArraysMission1
+{
+    internal class Program
+    {
+        enum Seasons
+        {
+            Spring,
+            Summer,
+            Autumn,
+            Winder,
+        }
+        static string CreateDayDescription(int day, int season, int year)
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
