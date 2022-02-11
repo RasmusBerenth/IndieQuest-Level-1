6382501affef90e690557006e096385c0c614181
RasmusBerenth <98969083+RasmusBerenth@users.noreply.github.com>
2022-02-11 17:21:17 +0100

    completed mission 2 off arrays

---
+++ arraysMission2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace arraysMission2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            
+            int[] monsters = new int[100];
+            
+            for (int i = 0; i < 100; i++)
+            {
+                int monstersPerLevel = random.Next(1, 51);
+                monsters[i] = monstersPerLevel;
+            }
+            
+            Array.Sort(monsters);
+            
+                Console.WriteLine($"Number of monsters in levels: {String.Join(" ", monsters)}");
+            
+
+
+        }
+    }
+}
