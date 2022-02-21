c29c81661073a5e6d9bdba94db38c0f40292ecc2
RasmusBerenth <98969083+RasmusBerenth@users.noreply.github.com>
2022-02-21 15:07:15 +0100

    Merge with mission 1

---
+++ FilesMission2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace FilesMission2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Starting text.
+            Console.WriteLine("Welcome to your biggest adventure yet!");
+            
+            //If the file dosen't exist previously go to "first run" and if it does got to "second run".
+            string playerNamePath = "player-name.txt";
+            if (File.Exists(playerNamePath))
+            {
+                string playerName = File.ReadAllText(playerNamePath);
+                //On the second run.
+                Console.WriteLine($"Welcome back, {playerName}, lets continue!");
+            }
+            else
+            {
+                Console.WriteLine("What is your name, traveler?");
+                string playerName = Console.ReadLine();
+                //Creating the file, player-name.txt, and storing the player name in it.
+
+                File.WriteAllText(playerNamePath, playerName);
+
+                //On the first run.
+                Console.WriteLine($"Nice to meet you {playerName}");
+
+            }
+
+            //Put the backers into an array.
+            string[] backers = File.ReadAllLines("backers.txt");
+            
+            //Display whether they can enter special area or not.
+
+
+        }
+    }
+}
