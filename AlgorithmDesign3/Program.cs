b6b2376dc6acda7a526d6d18f9fc3b1f38fd0d16
RasmusBerenth <98969083+RasmusBerenth@users.noreply.github.com>
2022-03-01 18:47:47 +0100

    started on Algorithm Design 3

---
+++ AlgorithmDesign3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AlgorithmDesign3
+{
+    internal class Program
+    {
+        static void DrawMap(int width, int height)
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            /* 
+            1. Prepear the data
+                1.1 Create code for the river
+                1.2 Create code for the road
+                1.3 Create code for the bridge
+            2. Draw the map
+                2.1 Draw the frame and title
+                2.2 Draw the road and side path
+                2.3 Draw the bridge
+                2.4 Draw the river
+                2.5 Draw the forest
+             */
+        }
+    }
+}
