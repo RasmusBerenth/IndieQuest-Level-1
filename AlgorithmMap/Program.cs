24e3e9f239a47fdb1d00ff4dda1168d6946010e6
RasmusBerenth <98969083+RasmusBerenth@users.noreply.github.com>
2022-03-03 16:05:56 +0100

    coding and drawing river

--- AlgorithmMap/Program.cs
+++ AlgorithmMap/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AlgorithmMap
 {
@@ -7,15 +8,43 @@ namespace AlgorithmMap
         static void DrawMap(int width, int height)
         {
             //1. Prepear the data
+            var random = new Random();
+
             //    1.1 Create code for the river
+            int riverStartPositionX = width * 3 / 4;
+            var riverPositionsX = new List<int>() { riverStartPositionX };
+
+            for (int y = 1; y < height; y++)
+            {
+                int riverDirection = random.Next(3);
+                int newPositionX;
+                int previousPositionX = riverPositionsX [y - 1];
+
+                //Left
+                if (riverDirection == 0)
+                {
+                    newPositionX = previousPositionX - 1;
+                }
+                //Right
+                else if (riverDirection == 1)
+                {
+                    newPositionX = previousPositionX + 1;
+                }
+                //Straight
+                else
+                {
+                    newPositionX = previousPositionX;
+                }
+                
+                riverPositionsX.Add(newPositionX);
+            }
 
             //    1.2 Create code for the road
 
             //    1.3 Create code for the bridge
-            
-            
+
+
             //2. Draw the map
-            var random = new Random();
 
             for (int y = 0; y < height; y++)
             {
@@ -43,8 +72,16 @@ namespace AlgorithmMap
 
 
                     //    2.2 Draw the road and side path
+
                     //    2.3 Draw the bridge
+
                     //    2.4 Draw the river
+                    if (x == riverPositionsX[y])
+                    {
+                        Console.Write("R");
+                        continue;
+                    }
+
                     //    2.5 Draw the forest
                     if (x < width/4)
                     {
@@ -67,10 +104,7 @@ namespace AlgorithmMap
         }
         static void Main(string[] args)
         {
-
-            
-            
-            DrawMap(100, 20);
+            DrawMap(40, 10);
         }
     }
 }
