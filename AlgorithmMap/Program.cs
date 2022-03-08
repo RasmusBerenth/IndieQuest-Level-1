1189cf397bf26dc1d227b7b35de5f332b68a5039
RasmusBerenth <98969083+RasmusBerenth@users.noreply.github.com>
2022-03-08 15:51:18 +0100

    completed Algorith design 4

--- AlgorithmMap/Program.cs
+++ AlgorithmMap/Program.cs
@@ -9,15 +9,17 @@ namespace AlgorithmMap
         {
             //1. Prepear the data
             var random = new Random();
-            int direction;
+            var treeSymbols = new string[] { "T", "@", "I" };
+            string title = "Adventure Map";
 
             //    1.1 Create code for the river
             int riverStartPositionX = width * 3 / 4;
-            var riverPositionsX = new List<int>() { riverStartPositionX };
+            var riverPositionsX = new List<int>() {riverStartPositionX};
+            var riverDirections = new List<int>();
 
             for (int y = 1; y < height; y++)
             {
-                direction = random.Next(3);
+                int direction = random.Next(3);
                 int newPositionX;
                 int previousPositionX = riverPositionsX[y - 1];
 
@@ -36,8 +38,9 @@ namespace AlgorithmMap
                 {
                     newPositionX = previousPositionX;
                 }
-
+                
                 riverPositionsX.Add(newPositionX);
+                riverDirections.Add(direction);
             }
 
             //    1.2 Create code for the road
@@ -46,7 +49,7 @@ namespace AlgorithmMap
 
             for (int x = 1; x < width; x++)
             {
-                direction = random.Next(3);
+                int direction = random.Next(7);
                 int newPositionY;
                 int previousPositionY = roadPositionsY[x - 1];
                 //Up
@@ -80,6 +83,7 @@ namespace AlgorithmMap
             }
             while (roadPositionX < riverPositionX);
             int intersectionX = roadPositionX;
+            
             //Calculate bridge parameters
             int bridgeStartX = intersectionX - 3;
             int bridgeY = roadPositionsY[bridgeStartX];
@@ -94,7 +98,7 @@ namespace AlgorithmMap
             //Remake the road after the bridge
             for (int x = bridgeEndX + 1; x < width; x++)
             {
-                direction = random.Next(3);
+                int direction = random.Next(3);
                 int newPositionY;
                 int previousPositionY = roadPositionsY[x - 1];
                 //Up
@@ -117,13 +121,13 @@ namespace AlgorithmMap
             }
 
             //2. Draw the map
-
             for (int y = 0; y < height; y++)
             {
 
                 for (int x = 0; x < width; x++)
                 {
-                    //    2.1 Draw the frame and title
+                    //    2.1 Draw the frame
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
                     if ((x == 0 && y == 0) || (x == width - 1 && y == 0) || (x == 0 && y == height - 1) || (x == width - 1 && y == height - 1))
                     {
                         Console.Write("+");
@@ -142,43 +146,53 @@ namespace AlgorithmMap
                         continue;
                     }
 
+                    //Draw title
+                    if (x == width / 2 - title.Length / 2 && y == 1)
+                    {
+                        Console.Write(title);
+                        x += title.Length -1;
+                        continue;
+                    }
 
                     //    2.2 Draw the road and side path
+                    Console.ForegroundColor= ConsoleColor.White;
                     if (y == roadPositionsY[x])
                     {
                         Console.Write("#");
                         continue;
                     }
 
-                    //if (x == riverPositionsX[y] - 5 && y > bridgeStartX)
-                    //{
-                    //    Console.Write("#");
-                    //    continue;
-                    //}
+                    if (y >= bridgeY && x == riverPositionsX[y] - 5)
+                    {
+                        Console.Write("#");
+                        continue;
+                    }
 
                     //    2.3 Draw the bridge
-                    if (y == bridgeY - 1 && x >= bridgeStartX && x <= bridgeEndX)
+                    if (y == bridgeY - 1 && x >= bridgeStartX + 1 && x <= bridgeEndX - 1)
                     {
                         Console.Write("=");
                         continue;
                     }
 
-                    if (y == bridgeY + 1 && x >= bridgeStartX && x <= bridgeEndX)
+                    if (y == bridgeY + 1 && x >= bridgeStartX + 1 && x <= bridgeEndX - 1)
                     {
                         Console.Write("=");
                         continue;
                     }
 
                     //    2.4 Draw the river
+                    Console.ForegroundColor = ConsoleColor.Blue;
                     if ((x == riverPositionsX[y]) || (x == riverPositionsX[y] + 1) || x == riverPositionsX[y] + 2)
                     {
+                        int direction = riverDirections[y];
                         if (direction == 2)
                         {
                             Console.Write("|");
                         }
                         else if (direction == 1)
                         {
-                            Console.Write("");
+                            Console.Write(@"\");
                         }
                         else
                         {
@@ -189,13 +203,15 @@ namespace AlgorithmMap
                     }
 
                     //    2.5 Draw the forest
+                    Console.ForegroundColor = ConsoleColor.Green;
                     if (x < width / 4)
                     {
                         int treeRoll = random.Next(100);
                         double treeChance = (Math.Sin((x / (width / 4.0) - 2) * Math.PI / 2) + 1) * 80;
                         if (treeChance > treeRoll)
                         {
-                            Console.Write("T");
+                            int randomTreeSymbolIndex = random.Next(treeSymbols.Length);
+                            Console.Write(treeSymbols[randomTreeSymbolIndex]);
                             continue;
                         }
                     }
