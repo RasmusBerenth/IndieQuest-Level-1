c0140d264e780645201c177d7334ffdda0b2e629
RasmusBerenth <98969083+RasmusBerenth@users.noreply.github.com>
2022-03-09 18:11:15 +0100

    worked on algorithm design 5

--- AlgorithmMap/Program.cs
+++ AlgorithmMap/Program.cs
@@ -5,49 +5,39 @@ namespace AlgorithmMap
 {
     internal class Program
     {
-        static void DrawMap(int width, int height)
+        static void GenerateCrossing()
         {
-            //1. Prepear the data
-            var random = new Random();
-            var treeSymbols = new string[] { "T", "@", "I" };
-            string title = "Adventure Map";
-
-            //    1.1 Create code for the river
-            int riverStartPositionX = width * 3 / 4;
-            var riverPositionsX = new List<int>() {riverStartPositionX};
-            var riverDirections = new List<int>();
-
-            for (int y = 1; y < height; y++)
+            //Find road river intersection
+            int roadPositionX = 0;
+            int riverPositionX;
+            do
             {
-                int direction = random.Next(3);
-                int newPositionX;
-                int previousPositionX = riverPositionsX[y - 1];
+                roadPositionX++;
+                int roadPositionY = roadPositionsY[roadPositionX];
+                riverPositionX = riverPositionsX[roadPositionY];
+            }
+            while (roadPositionX < riverPositionX);
+            int intersectionX = roadPositionX;
 
-                //Left
-                if (direction == 0)
-                {
-                    newPositionX = previousPositionX - 1;
-                }
-                //Right
-                else if (direction == 1)
-                {
-                    newPositionX = previousPositionX + 1;
-                }
-                //Straight
-                else
-                {
-                    newPositionX = previousPositionX;
-                }
-                
-                riverPositionsX.Add(newPositionX);
-                riverDirections.Add(direction);
+            //Calculate bridge parameters
+            int bridgeStartX = intersectionX - 3;
+            int bridgeY = roadPositionsY[bridgeStartX];
+            int bridgeEndX = bridgeStartX + 9;
+
+            //Make road go straight across the bridge
+            for (int bridgeX = bridgeStartX; bridgeX <= bridgeEndX && bridgeX < width; bridgeX++)
+            {
+                roadPositionsY[bridgeX] = bridgeY;
             }
 
-            //    1.2 Create code for the road
-            int roadStartPositionY = height / 2;
-            var roadPositionsY = new List<int>() { roadStartPositionY };
+            //Remake the road after the bridge
 
-            for (int x = 1; x < width; x++)
+            GenerateRoad(bridgeEndX + 1, width, height, roadPositionsY);
+        }
+        static void GenerateRoad(int roadStartX, int width, int height, List<int> roadPositionsY)
+        {
+            var random = new Random();
+            for (int x = roadStartX; x < width; x++)
             {
                 int direction = random.Next(7);
                 int newPositionY;
@@ -68,11 +58,94 @@ namespace AlgorithmMap
                     newPositionY = previousPositionY;
                 }
 
-                roadPositionsY.Add(newPositionY);
+                if (roadPositionsY.Count > x)
+                {
+                    roadPositionsY[x] = newPositionY;
+                }
+                else
+                {
+                    roadPositionsY.Add(newPositionY);
+                }
             }
+        }
+        static void GenerateCurve(int startPositionX, int height, int curveChance, out List<int> positionsX, out List<int> directions)
+        {
+            var random = new Random();
+
+            positionsX = new List<int>() { startPositionX };
+            directions = new List<int>();
+
+            for (int y = 1; y < height; y++)
+            {
+                int direction = random.Next(curveChance);
+                int newPositionX;
+                int previousPositionX = positionsX[y - 1];
+
+                //Left
+                if (direction == 0)
+                {
+                    newPositionX = previousPositionX - 1;
+                }
+                //Right
+                else if (direction == 1)
+                {
+                    newPositionX = previousPositionX + 1;
+                }
+                //Straight
+                else
+                {
+                    newPositionX = previousPositionX;
+                }
+
+                positionsX.Add(newPositionX);
+                directions.Add(direction);
+            }
+        }
+        static void DrawCurve(List<int> directions, int y)
+        {
+            int direction = directions[y];
+            if (direction == 0)
+            {
+                Console.Write("/");
+            }
+            else if (direction == 1)
+            {
+                Console.Write(@"\");
+            }
+            else
+            {
+                Console.Write("|");
+            }
+        }
+        static void DrawMap(int width, int height)
+        {
+            //1. Prepear the data
+            var random = new Random();
+            var treeSymbols = new string[] { "T", "@", "I" };
+            string title = "Adventure Map";
+
+            //    1.1 Create code for the river
+            int riverStartPositionX = width * 3 / 4;
+            List<int> riverPositionsX;
+            List<int> riverDirections;
+
+            GenerateCurve(riverStartPositionX, height, 3, out riverPositionsX, out riverDirections);
+
+            // Create code for the wall
+            int wallStartPositionX = width / 4;
+            List<int> wallPositionsX;
+            List<int> wallDirections;
+
+            GenerateCurve(wallStartPositionX, height, 6, out wallPositionsX, out wallDirections);
+
+            //    1.2 Create code for the road
+            int roadStartPositionY = height / 2;
+            var roadPositionsY = new List<int>() { roadStartPositionY };
+
+            GenerateRoad(1, width, height, roadPositionsY);
 
             //    1.3 Create code for the bridge
-            //Find intersection
+            //Find road river intersection
             int roadPositionX = 0;
             int riverPositionX;
             do
@@ -83,7 +156,7 @@ namespace AlgorithmMap
             }
             while (roadPositionX < riverPositionX);
             int intersectionX = roadPositionX;
-            
+
             //Calculate bridge parameters
             int bridgeStartX = intersectionX - 3;
             int bridgeY = roadPositionsY[bridgeStartX];
@@ -96,29 +169,9 @@ namespace AlgorithmMap
             }
 
             //Remake the road after the bridge
-            for (int x = bridgeEndX + 1; x < width; x++)
-            {
-                int direction = random.Next(3);
-                int newPositionY;
-                int previousPositionY = roadPositionsY[x - 1];
-                //Up
-                if (direction == 0 && previousPositionY > 2)
-                {
-                    newPositionY = previousPositionY - 1;
-                }
-                //Down
-                else if (direction == 1 && previousPositionY < height - 3)
-                {
-                    newPositionY = previousPositionY + 1;
-                }
-                //Straight
-                else
-                {
-                    newPositionY = previousPositionY;
-                }
 
-                roadPositionsY[x] = newPositionY;
-            }
+            GenerateRoad(bridgeEndX + 1, width, height, roadPositionsY);
+
 
             //2. Draw the map
             for (int y = 0; y < height; y++)
@@ -150,12 +203,12 @@ namespace AlgorithmMap
                     if (x == width / 2 - title.Length / 2 && y == 1)
                     {
                         Console.Write(title);
-                        x += title.Length -1;
+                        x += title.Length - 1;
                         continue;
                     }
 
                     //    2.2 Draw the road and side path
-                    Console.ForegroundColor= ConsoleColor.White;
+                    Console.ForegroundColor = ConsoleColor.White;
                     if (y == roadPositionsY[x])
                     {
                         Console.Write("#");
@@ -183,24 +236,22 @@ namespace AlgorithmMap
 
                     //    2.4 Draw the river
                     Console.ForegroundColor = ConsoleColor.Blue;
-                    if ((x == riverPositionsX[y]) || (x == riverPositionsX[y] + 1) || x == riverPositionsX[y] + 2)
+                    if ((x == riverPositionsX[y]) || (x == riverPositionsX[y] + 1) || (x == riverPositionsX[y] + 2))
                     {
-                        int direction = riverDirections[y];
-                        if (direction == 2)
-                        {
-                            Console.Write("|");
-                        }
-                        else if (direction == 1)
-                        {
-                            Console.Write(@"\");
-                        }
-                        else
-                        {
-                            Console.Write("/");
-                        }
+                        DrawCurve(riverDirections, y);
                         continue;
-
                     }
+
+                    //Drawing Wall
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    if ((x == wallPositionsX[y]) || (x == wallPositionsX[y] + 1))
+                    {
+                        DrawCurve(wallDirections, y);
+                        continue;
+                    }
+
+                    //Drawing towers
+
 
                     //    2.5 Draw the forest
                     Console.ForegroundColor = ConsoleColor.Green;
