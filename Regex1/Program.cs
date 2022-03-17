d764fc214c4b38eb60eb9bcdb15d1ff1ade10b90
RasmusBerenth <98969083+RasmusBerenth@users.noreply.github.com>
2022-03-17 18:17:00 +0100

    regex 1 mission 3 and 4

--- Regex1/Program.cs
+++ Regex1/Program.cs
@@ -12,13 +12,32 @@ namespace Regex1
             string monsterManualText = File.ReadAllText(monsterManualPath);
             string[] monsterStatBlocks = monsterManualText.Split("\n\n");
 
-
+            Console.WriteLine("Can fly 10 - 40 feet per turn:");
             foreach (string monsterStatBlock in monsterStatBlocks)
             {
                 string[] monsterStatLines = monsterStatBlock.Split("\n");
                 string monsterName = monsterStatLines[0];
-                bool manualSearch = Regex.IsMatch(monsterStatBlock, "fly");
-                Console.WriteLine($"{monsterName} - Can fly: {manualSearch}");
+                //Solar, Planetar, Roc 100+(del)
+                if (Regex.IsMatch(monsterStatBlock, "fly [1,2,3,4]"))
+                {
+                    Console.WriteLine(monsterName);
+                }
+
+
+
+
+
+                //string[] monsterHitPoint = monsterStatLines[2].Split("(");
+                //if (monsterHitPoint.Length == 2)
+                //{
+                //    bool manualSearch = Regex.IsMatch(monsterHitPoint[1], "\\d{2}d");
+                //    Console.WriteLine($"{monsterName} - 10+ dice rolls: {manualSearch}");
+                //}
+                //else
+                //{
+                //    Console.WriteLine($"{monsterName} - 10+ dice rolls: False");
+                //}
+
             }
 
 
