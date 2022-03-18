bf3895db37ba494b8eb317c84197b61144324c38
RasmusBerenth <98969083+RasmusBerenth@users.noreply.github.com>
2022-03-18 16:40:41 +0100

    Regex 2 mission 3

---
+++ Regex2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Regex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string monsterManualPath = "MonsterManual.txt";
+            string monsterManualText = File.ReadAllText(monsterManualPath);
+            string[] monsterStatBlocks = monsterManualText.Split("\n\n");
+
+            foreach (string monsterStatBlock in monsterStatBlocks)
+            {
+                string[] monsterStatLines = monsterStatBlock.Split("\n");
+                string monsterName = monsterStatLines[0];
+                string monsterStatBlockSecondLine = monsterStatLines[1];
+                string[] monsterSizeAndAlignment = monsterStatBlockSecondLine.Split(", ");
+                string monsterAlignment = monsterSizeAndAlignment[1];
+                if (Regex.IsMatch(monsterAlignment, "(lawful|chaotic|neutral|good|evil)"))
+                {
+                    Console.WriteLine($"{monsterName} ({monsterAlignment})");
+                }
+
+            }
+        }
+    }
+}
