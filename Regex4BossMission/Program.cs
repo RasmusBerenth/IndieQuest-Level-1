ed521de8a9f33340fac56675f45dc458e5b5426d
RasmusBerenth <98969083+RasmusBerenth@users.noreply.github.com>
2022-03-22 19:00:54 +0100

    start of regex 4 part 2

---
+++ Regex4BossMission/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+
+namespace Regex4BossMission
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var httpClient = new HttpClient();
+            string htmlCode = httpClient.GetStringAsync(@"https://store.steampowered.com/app/814680/Unbound_Worlds_Apart/").Result;
+            Match result = Regex.Match(htmlCode, "(<\\w+\\s\\w+=\"\\w+\\s\\w+\"\\s\\w+=\"\\w+\">(\\S+\\s\\w+)<\\/\\w+>)");
+            if (result.Success)
+            {
+                Console.WriteLine($"The rating of the game Unbound: Worlds Apart is {result}");
+            }
+
+        }
+    }
+}
