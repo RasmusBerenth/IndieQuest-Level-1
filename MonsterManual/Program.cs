b622162ed77225ecc26c394f3678edb4f29167e5
RasmusBerenth <98969083+RasmusBerenth@users.noreply.github.com>
2022-03-28 13:52:49 +0200

    finished monster manual 2

--- MonsterManual/Program.cs
+++ MonsterManual/Program.cs
@@ -11,12 +11,12 @@ namespace MonsterManual
         public string Description;
         public string Alignment;
         public string HitPoint;
-        public List<MonsterEntry> MonsterEntries = new List<MonsterEntry>();
     }
     internal class Program
     {
         static void DisplayMonster(MonsterEntry monster)
         {
+            Console.Clear();
             Console.WriteLine(monster.Name);
             Console.WriteLine(monster.Description);
             Console.WriteLine(monster.Alignment);
@@ -24,8 +24,8 @@ namespace MonsterManual
         }
         static void Main(string[] args)
         {
+            //Creating the list of monsters from the monster manual.
             var monsterEntries = new List<MonsterEntry>();
-
             string monsterManualPath = "MonsterManual.txt";
             string monsterManualText = File.ReadAllText(monsterManualPath);
             string[] monsterStatBlocks = monsterManualText.Split("\n\n");
@@ -48,9 +48,51 @@ namespace MonsterManual
 
             }
 
-            DisplayMonster(monsterEntries[8]);
+            //Prompt for user input.
+            Console.WriteLine("MONSTER MANUAL");
+            Console.WriteLine("Eneter a query to search monster by name:");
 
+            do
+            {
+                string userInput = Console.ReadLine();
 
+                //Output a list of numbered monster matching input.
+                int counter = 0;
+                var matchedMonsters = new List<MonsterEntry>();
+                foreach (MonsterEntry monsterEntry in monsterEntries)
+                {
+                    Match result = Regex.Match(monsterEntry.Name, userInput, RegexOptions.IgnoreCase);
+                    if (result.Success)
+                    {
+                        counter++;
+                        Console.WriteLine($"{counter}. {monsterEntry.Name}");
+                        matchedMonsters.Add(monsterEntry);
+                    }
+                }
+
+                //ask for specific number, user picks number.
+                if (counter > 1)
+                {
+                    Console.WriteLine("Which monster would you like to look up?");
+                    int chosenMonsterNumber = Convert.ToInt32(Console.ReadLine());
+                    //Confirm monster.
+                    MonsterEntry chosenMonster = matchedMonsters[chosenMonsterNumber - 1];
+                    DisplayMonster(chosenMonster);
+                    break;
+                }
+                else if (counter == 0)
+                {
+                    //If no monster found in original search ask for new input.
+                    Console.WriteLine("No found monsters, try again");
+                }
+                else
+                {
+                    //If only one monster was found display info, skipping the confirmation of the monster.
+                    DisplayMonster(matchedMonsters[0]);
+                    break;
+                }
+
+            } while (true);
         }
     }
 }
