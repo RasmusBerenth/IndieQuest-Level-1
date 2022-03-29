3352a94823d300692835f56244ead12e76a34fda
RasmusBerenth <98969083+RasmusBerenth@users.noreply.github.com>
2022-03-29 16:54:05 +0200

    worked on monster manual 3

--- MonsterManual/Program.cs
+++ MonsterManual/Program.cs
@@ -5,10 +5,23 @@ using System.Text.RegularExpressions;
 
 namespace MonsterManual
 {
+    enum ArmorType
+    {
+        Unspecified,
+        Natural,
+        Leather,
+        StuddedLeather,
+        Hide,
+        ChainShirt,
+        ChainMail,
+        ScaleMail,
+        Plate,
+        Other
+    }
     class ArmorInformation
     {
         public int Class;
-        public string Type;
+        public ArmorType Type;
     }
     class MonsterEntry
     {
@@ -46,64 +59,175 @@ namespace MonsterManual
                 Match monsterDesciption = Regex.Match(monsterStatLines[1], "(.*),");
                 Match monsterAlignment = Regex.Match(monsterStatLines[1], ", (.*)");
                 Match monsterHitPont = Regex.Match(monsterStatLines[2], "\\d+d\\d+(\\+\\d+)?");
-                Match monsterArmor = Regex.Match(monsterStatLines[3], "Armor Class: (\\d+) (\\((.*)\\))?");
-
-                Convert.ToInt32(monsterArmor); //??? Parse, TryParse
+                Match monsterArmor = Regex.Match(monsterStatLines[3], "Armor Class: (\\d+) (?:\\((.*)\\))?");
 
                 monster.Name = monsterName;
                 monster.Description = monsterDesciption.Groups[1].Value;
                 monster.Alignment = monsterAlignment.Groups[1].Value;
                 monster.HitPoint = monsterHitPont.Value;
-                monster.Armor.Type = monsterArmor.Groups[3].Value;
-                monster.Armor.Class = monsterArmor.Groups[1].Value;
+                if (monsterArmor.Groups[2].Success)
+                {
+                    string armorTypeText = monsterArmor.Groups[2].Value;
+
+                    if (Regex.IsMatch(armorTypeText, "natural", RegexOptions.IgnoreCase))
+                    {
+                        monster.Armor.Type = ArmorType.Natural;
+                    }
+                    else if (Regex.IsMatch(armorTypeText, "^[studded]leather", RegexOptions.IgnoreCase))
+                    {
+                        monster.Armor.Type = ArmorType.Leather;
+                    }
+                    else if (Regex.IsMatch(armorTypeText, "studded leather", RegexOptions.IgnoreCase))
+                    {
+                        monster.Armor.Type = ArmorType.StuddedLeather;
+                    }
+                    else if (Regex.IsMatch(armorTypeText, "hide", RegexOptions.IgnoreCase))
+                    {
+                        monster.Armor.Type = ArmorType.Hide;
+                    }
+                    else if (Regex.IsMatch(armorTypeText, "chain shirt", RegexOptions.IgnoreCase))
+                    {
+                        monster.Armor.Type = ArmorType.ChainShirt;
+                    }
+                    else if (Regex.IsMatch(armorTypeText, "chain mail", RegexOptions.IgnoreCase))
+                    {
+                        monster.Armor.Type = ArmorType.ChainMail;
+                    }
+                    else if (Regex.IsMatch(armorTypeText, "scale mail", RegexOptions.IgnoreCase))
+                    {
+                        monster.Armor.Type = ArmorType.ScaleMail;
+                    }
+                    else if (Regex.IsMatch(armorTypeText, "plate", RegexOptions.IgnoreCase))
+                    {
+                        monster.Armor.Type = ArmorType.Plate;
+                    }
+                    else
+                    {
+                        monster.Armor.Type = ArmorType.Other;
+                    }
+                }
+                else
+                {
+                    monster.Armor.Type = ArmorType.Unspecified;
+                }
+                monster.Armor.Class = Convert.ToInt32(monsterArmor.Groups[1].Value);
                 monsterEntries.Add(monster);
             }
 
-            //Prompt for user input.
+            //Prompt for user input and let user chose between name and armor. (n or a)
             Console.WriteLine("MONSTER MANUAL");
-            Console.WriteLine("Eneter a query to search monster by name:");
-
-            do
+            Console.WriteLine("Would you like to search by (n)ame or (a)rmor");
+            string searchMethod = Console.ReadLine();
+            var matchedMonsters = new List<MonsterEntry>();
+            if (searchMethod == "n")
             {
-                string userInput = Console.ReadLine();
+                //Output chosen search method.
+                Console.WriteLine("Eneter a query to search monster by name:");
+                do
+                {
+                    string userInput = Console.ReadLine();
 
-                //Output a list of numbered monster matching input.
+                    //Output a list of numbered monster matching input.
+                    int counter = 0;
+                    foreach (MonsterEntry monsterEntry in monsterEntries)
+                    {
+                        Match result = Regex.Match(monsterEntry.Name, userInput, RegexOptions.IgnoreCase);
+                        if (result.Success)
+                        {
+                            counter++;
+                            Console.WriteLine($"{counter}. {monsterEntry.Name}");
+                            matchedMonsters.Add(monsterEntry);
+                        }
+                    }
+
+                    //ask for specific number, user picks number.
+                    if (counter > 1)
+                    {
+                        Console.WriteLine("Which monster would you like to look up?");
+                        int chosenMonsterNumber = Convert.ToInt32(Console.ReadLine());
+                        //Confirm monster.
+                        MonsterEntry chosenMonster = matchedMonsters[chosenMonsterNumber - 1];
+                        DisplayMonster(chosenMonster);
+                        break;
+                    }
+                    else if (counter == 0)
+                    {
+                        //If no monster found in the search ask for new input.
+                        Console.WriteLine("No found monsters, try again");
+                    }
+                    else
+                    {
+                        //If only one monster was found display info.
+                        DisplayMonster(matchedMonsters[0]);
+                        break;
+                    }
+
+                } while (true);
+            }
+            else if (searchMethod == "a")
+            {
+                //Output chosen search method.
+                Console.WriteLine("Enter an armor type to search by:");
+
+                //Display list of numbered armor types.
+                string[] armorTypeNames = Enum.GetNames<ArmorType>();
                 int counter = 0;
-                var matchedMonsters = new List<MonsterEntry>();
+                foreach (string armorTypeName in armorTypeNames)
+                {
+                    counter++;
+                    Console.WriteLine($"{counter}. {armorTypeName}");
+                }
+
+                //Select armor type by number.
+                int selectedArmorTypeNumber = Convert.ToInt32(Console.ReadLine());
+                ArmorType[] armorTypes = Enum.GetValues<ArmorType>();
+                ArmorType selectedArmorType = armorTypes[selectedArmorTypeNumber - 1];
+
+                //Display monsters with selected armor type.
                 foreach (MonsterEntry monsterEntry in monsterEntries)
                 {
-                    Match result = Regex.Match(monsterEntry.Name, userInput, RegexOptions.IgnoreCase);
-                    if (result.Success)
+                    if (monsterEntry.Armor.Type == selectedArmorType)
                     {
                         counter++;
                         Console.WriteLine($"{counter}. {monsterEntry.Name}");
                         matchedMonsters.Add(monsterEntry);
                     }
+
                 }
 
-                //ask for specific number, user picks number.
+                //Select monster.
                 if (counter > 1)
                 {
+                    Console.Clear();
                     Console.WriteLine("Which monster would you like to look up?");
+                    counter = 0;
+                    foreach (MonsterEntry monsterEntry in matchedMonsters)
+                    {
+                        counter++;
+                        Console.WriteLine($"{counter}. {monsterEntry.Name}");
+                    }
+
                     int chosenMonsterNumber = Convert.ToInt32(Console.ReadLine());
                     //Confirm monster.
                     MonsterEntry chosenMonster = matchedMonsters[chosenMonsterNumber - 1];
                     DisplayMonster(chosenMonster);
-                    break;
                 }
                 else if (counter == 0)
                 {
-                    //If no monster found in original search ask for new input.
+                    //If no monster found in the search ask for new input.
                     Console.WriteLine("No found monsters, try again");
                 }
                 else
                 {
-                    //If only one monster was found display info, skipping the confirmation of the monster.
+                    //If only one monster was found display info.
                     DisplayMonster(matchedMonsters[0]);
-                    break;
                 }
 
-            } while (true);
+            }
+            else
+            {
+                Console.WriteLine("This wasen't an option...");
+            }
         }
     }
 }
